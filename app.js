02cb426858e2f30f96e58b186109c52002530882
arczi0 <arczyy@outlook.com>
2020-04-01 23:53:53 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+fetch('https://corona.lmao.ninja/countries/poland')
+  .then(response => {
+    return response.json()
+  })
+  .then(data => {
+    var country = (data.country);
+    var cases = (data.cases);
+    var deaths = (data.deaths);
+    var todaycases = (data.todayCases);
+    var todaydeaths = (data.todayDeaths);
+    //document.getElementById("country").innerHTML = "Kraj: " + country;
+    document.getElementById("cases").innerHTML = "Liczba zakażonych: " + "<br>" + cases;
+    document.getElementById("deaths").innerHTML = "Liczba zgonów: " + "<br>"+ deaths;
+    document.getElementById("todayCases").innerHTML =  todaycases;
+    document.getElementById("todayDeaths").innerHTML =  todaydeaths;
+  })
+  .catch(err => {
+    console.log("Data download error")
+  })
+
